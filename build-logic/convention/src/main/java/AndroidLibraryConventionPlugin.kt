77083e9ea6d1eb6fc790188cb728d87dd8af2c02
enso <>
2025-02-16 22:51:22 +0900

    build-logic 설정 파일 추가

---
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,45 @@
+import com.android.build.api.variant.LibraryAndroidComponentsExtension
+import com.android.build.gradle.LibraryExtension
+import com.enso.convention.configureFlavors
+import com.enso.convention.configureGradleManagedDevices
+import com.enso.convention.configureKotlinAndroid
+import com.enso.convention.configurePrintApksTask
+import com.enso.convention.disableUnnecessaryAndroidTests
+import com.enso.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            extensions.configure<LibraryExtension> {
+                configureKotlinAndroid(this)
+                defaultConfig.targetSdk = 34
+                defaultConfig.testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                testOptions.animationsDisabled = true
+                configureFlavors(this)
+                configureGradleManagedDevices(this)
+                resourcePrefix = path.split("""\W""".toRegex()).drop(1).distinct().joinToString(separator = "_").lowercase() + "_"
+            }
+
+            extensions.configure<LibraryAndroidComponentsExtension> {
+                configurePrintApksTask(this)
+                disableUnnecessaryAndroidTests(target)
+            }
+
+            dependencies {
+                add("androidTestImplementation", libs.findLibrary("kotlin.test").get())
+                add("testImplementation", libs.findLibrary("kotlin.test").get())
+
+                add("implementation", libs.findLibrary("androidx.tracing.ktx").get())
+            }
+        }
+    }
+}
--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.lafi.android.library)
 }
 
 android {
-    namespace = ""
+    namespace = "com.enso.login"
 }
 
 dependencies {
