c351b7e9b49d04630b49e432d89071b9a367ddfd
enso\bbqck <kukwonho@gmail.com>
2025-03-07 22:29:30 +0900

    SignupViewModel 생성 및 바인드

--- feature/signup/src/main/java/com/lafi/lawyer/feature/signup/SignupActivity.kt
+++ feature/signup/src/main/java/com/lafi/lawyer/feature/signup/SignupActivity.kt
@@ -1,13 +1,16 @@
 package com.lafi.lawyer.feature.signup
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.viewModels
 import com.lafi.lawyer.core.design_system.activity.BaseActivity
 import com.lafi.lawyer.feature.signup.databinding.ActivitySignupBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignupActivity : BaseActivity<ActivitySignupBinding>(ActivitySignupBinding::inflate) {
+    private val vm by viewModels<SignupViewModel>()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
---
+++ feature/signup/src/main/java/com/lafi/lawyer/feature/signup/SignupViewModel.kt
@@ -0,0 +1,8 @@
+package com.lafi.lawyer.feature.signup
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SignupViewModel : ViewModel() {
+}
