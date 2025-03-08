9e82026d4b348b8298d79545dc55fa2d22142ec9
enso\bbqck <kukwonho@gmail.com>
2025-03-08 19:55:44 +0900

    인젝션 추가

--- feature/signup/src/main/java/com/lafi/lawyer/feature/signup/SignupViewModel.kt
+++ feature/signup/src/main/java/com/lafi/lawyer/feature/signup/SignupViewModel.kt
@@ -2,7 +2,11 @@ package com.lafi.lawyer.feature.signup
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class SignupViewModel : ViewModel() {
+class SignupViewModel @Inject constructor(
+
+) : ViewModel() {
+    
 }
