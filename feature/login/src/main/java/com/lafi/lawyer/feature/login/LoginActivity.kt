361c80214a1f2e67399f73409b2360425f930bcc
enso\bbqck <kukwonho@gmail.com>
2025-04-25 23:32:01 +0900

    임시 커밋

--- feature/login/src/main/java/com/lafi/lawyer/feature/login/LoginActivity.kt
+++ feature/login/src/main/java/com/lafi/lawyer/feature/login/LoginActivity.kt
@@ -117,7 +117,7 @@ class LoginActivity : BaseActivity<FeatureLoginActivityLoginBinding>(FeatureLogi
             }
         } else {
             Log.d("whk__", "카카오 왜? : ${error?.message}")
-            Toast.makeText(this@LoginActivity, "카카오 로그인 실패", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@LoginActivity, "카카오 로그인 실패 ${error?.message}", Toast.LENGTH_SHORT).show()
         }
     }
 
