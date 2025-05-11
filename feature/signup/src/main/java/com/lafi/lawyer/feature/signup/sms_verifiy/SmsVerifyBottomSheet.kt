52adc95e184a625aa11462d84dc84a3a5c6c72d3
enso <kukwonho@gmail.com>
2025-05-11 23:55:49 +0900

    문자인증 스케일리플 적용

--- feature/signup/src/main/java/com/lafi/lawyer/feature/signup/sms_verifiy/SmsVerifyBottomSheet.kt
+++ feature/signup/src/main/java/com/lafi/lawyer/feature/signup/sms_verifiy/SmsVerifyBottomSheet.kt
@@ -1,9 +1,21 @@
 package com.lafi.lawyer.feature.signup.sms_verifiy
 
+import android.os.Bundle
+import android.view.View
+import com.lafi.lawyer.core.design_system.component.scale_ripple.setOnScaleClickListener
 import com.lafi.lawyer.core.design_system.fragment.BaseBottomSheetFragment
 import com.lafi.lawyer.feature.signup.databinding.FeatureSignupSmsVerifyBottomSheetBinding
 
 class SmsVerifyBottomSheet(override val TAG: String = SmsVerifyBottomSheet.TAG) : BaseBottomSheetFragment<FeatureSignupSmsVerifyBottomSheetBinding>(FeatureSignupSmsVerifyBottomSheetBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.tvContinue.setOnScaleClickListener(96) {}
+    }
 
     companion object {
         const val TAG = "SmsVerifyBottomSheet"
