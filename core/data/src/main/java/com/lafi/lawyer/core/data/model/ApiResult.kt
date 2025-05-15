8e9c9500df315c20c26880784df1a2e183bc3fff
enso <kukwonho@gmail.com>
2025-05-15 20:20:58 +0900

    맵퍼 확장함수 작성 완료

--- core/data/src/main/java/com/lafi/lawyer/core/data/model/ApiResult.kt
+++ core/data/src/main/java/com/lafi/lawyer/core/data/model/ApiResult.kt
@@ -1,8 +1,23 @@
 package com.lafi.lawyer.core.data.model
 
+import com.lafi.lawyer.core.domain.model.DataResult
 import com.lafi.lawyer.core.model.common.data.ErrorData
 
 sealed class ApiResult<out T> {
     data class Success<T>(val data: T): ApiResult<T>()
     data class Error(val error: ErrorData): ApiResult<Nothing>()
+}
+
+fun <T, R> ApiResult<T>.map(transform: (T) -> R): DataResult<R> {
+    return when (this) {
+        is ApiResult.Success -> DataResult.Success(transform(this.data))
+        is ApiResult.Error -> DataResult.Error(this.error)
+    }
+}
+
+fun <T> ApiResult<T>.map(): DataResult<T> {
+    return when (this) {
+        is ApiResult.Success -> DataResult.Success(this.data)
+        is ApiResult.Error -> DataResult.Error(this.error)
+    }
 }
--- core/data/src/main/java/com/lafi/lawyer/core/data/repository/AuthRepositoryImpl.kt
+++ core/data/src/main/java/com/lafi/lawyer/core/data/repository/AuthRepositoryImpl.kt
@@ -1,10 +1,10 @@
 package com.lafi.lawyer.core.data.repository
 
 import com.lafi.lawyer.core.data.datasource.AuthRemoteDataSource
-import com.lafi.lawyer.core.data.model.ApiResult
 import com.lafi.lawyer.core.data.model.auth.request.AuthLoginSocialRequest
 import com.lafi.lawyer.core.data.model.auth.request.SmsVerifyRequest
 import com.lafi.lawyer.core.data.model.auth.request.SmsVerifyRequestCodeRequest
+import com.lafi.lawyer.core.data.model.map
 import com.lafi.lawyer.core.domain.model.DataResult
 import com.lafi.lawyer.core.domain.model.auth.SmsVerifyRequestData
 import com.lafi.lawyer.core.domain.model.auth.SocialProvider
@@ -19,35 +19,25 @@ class AuthRepositoryImpl @Inject constructor(
         provider: SocialProvider,
         accessToken: String
     ): DataResult<String> {
-        val response = authRemoteDataSource.postLoginSocial(
+        return authRemoteDataSource.postLoginSocial(
             requestBody = AuthLoginSocialRequest(
                 provider = provider.providerName,
                 accessToken = accessToken,
                 requestID = "12345678901234567890121234567892"
             )
-        )
-
-        return when (response) {
-            is ApiResult.Success -> DataResult.Success(response.data.userID)
-            is ApiResult.Error -> DataResult.Error(response.error)
-        }
+        ).map { it.userID }
     }
 
     override suspend fun smsVerifyRequest(
         smsVerifyType: SmsVerifyType,
         phoneNumber: String
     ): DataResult<SmsVerifyRequestData> {
-        val response = authRemoteDataSource.postSmsVerifyRequestCode(
+        return authRemoteDataSource.postSmsVerifyRequestCode(
             requestBody = SmsVerifyRequestCodeRequest(
                 requestType = smsVerifyType.text,
                 phoneNumber = phoneNumber
             )
-        )
-
-        return when (response) {
-            is ApiResult.Success -> DataResult.Success(response.data.toDomain())
-            is ApiResult.Error -> DataResult.Error(response.error)
-        }
+        ).map { it.toDomain() }
     }
 
     override suspend fun smsVerify(
@@ -56,19 +46,13 @@ class AuthRepositoryImpl @Inject constructor(
         requestId: String,
         code: String
     ): DataResult<Unit> {
-        val response = authRemoteDataSource.postSmsVerify(
+        return authRemoteDataSource.postSmsVerify(
             requestBody = SmsVerifyRequest(
                 requestType = smsVerifyType.text,
                 phoneNumber = phoneNumber,
                 requestId = requestId,
                 code = code
             )
-        )
-
-        return when (response) {
-            is ApiResult.Success -> DataResult.Success(Unit)
-            is ApiResult.Error -> DataResult.Error(response.error)
-        }
+        ).map()
     }
 }
-
